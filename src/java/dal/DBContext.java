4a76d653a81317ce49ccbf3d4f911fea464d18c8
Kiên Trần Trung <kien030405@gmail.com>
2026-06-28 23:06:56 +0700

    Add DBContext class for database connection

---
+++ src/java/dal/DBContext.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Trung Kien
+ */
+public class DBContext {
+    protected Connection connection;
+    public DBContext() {
+        //@Students: You are not allowed to edit this method  
+        try {
+            Properties properties = new Properties();
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("../ConnectDB.properties");
+            try {
+                properties.load(inputStream);
+            } catch (IOException ex) {
+                Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            String user = properties.getProperty("userID");
+            String pass = properties.getProperty("password");
+            String url = properties.getProperty("url");
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+
